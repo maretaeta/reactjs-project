024e09d52fcd76c5b4a539b35d8b58205200dedc
unknown <maretanur197@gmail.com>
2024-09-07 23:42:31 +0800

    update menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -75,18 +75,19 @@ const Menu = () => {
   const [activeCategory, setActiveCategory] = useState("seasonal");
 
   return (
-    <div className="min-h-screen bg-white">
-      <div className=" p-10 mx-auto max-w-6xl">
-        <h1 className="text-3xl font-bold mb-20 text-center">Menu</h1>
-
-        <div className="flex justify-between border-b mb-6">
+    <div className="min-h-screen flex flex-col bg-white">
+      <div className="p-6 md:p-10 flex-grow">
+        <h1 className="text-base lg:text-lg font-bold mb-10 md:mb-20 text-center">
+          Menu
+        </h1>
+        <div className="flex flex-wrap justify-between border-b mb-6">
           {categories.map((category) => (
             <button
               key={category.id}
               onClick={() => setActiveCategory(category.id)}
-              className={`flex-1 py-4 px-6 text-center text-lg font-semibold transition-colors duration-300 ${
+              className={`w-full md:w-auto flex-1 py-4 px-4 md:px-6 text-center text-sm font-semibold transition-colors duration-300 ${
                 activeCategory === category.id
-                  ? "border-b-4 border-black text-black"
+                  ? "border-b-4 border-black text-black text-sm"
                   : "text-gray-500 hover:text-black"
               }`}
             >
@@ -95,18 +96,18 @@ const Menu = () => {
           ))}
         </div>
 
-        <div className="p-6">
+        <div className="flex flex-col gap-4 p-6">
           {menuItems[activeCategory].map((item, index) => (
             <div key={index} className="flex items-center mb-6">
               <img
                 src={item.image}
                 alt={item.name}
-                className="w-20 h-20 object-cover rounded-lg"
+                className="w-20 h-20 object-cover rounded-lg text-sm"
               />
               <div className="ml-6">
-                <h4 className="text-xl font-semibold">{item.name}</h4>
-                <p className="text-gray-500">{item.description}</p>
-                <p className="text-black mt-2 text-lg font-bold">
+                <h4 className="text-sm font-semibold">{item.name}</h4>
+                <p className="text-sm text-gray-500">{item.description}</p>
+                <p className="text-black mt-2 text-sm font-bold">
                   {item.price}
                 </p>
               </div>
@@ -114,7 +115,6 @@ const Menu = () => {
           ))}
         </div>
       </div>
-
       <Footer />
     </div>
   );
